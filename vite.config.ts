5fbba7d2e89c91cb7195ce86bba9721de5454141
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2025-02-12 07:10:41 +0700

    chore: remove `@types/node`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
